f603bb43e56d63015032fcd6e1d689698ec72fbc
xmagickit <silasforrest0322@gmail.com>
2024-11-05 10:50:50 -0600

    scrape schedule

--- R/data/schedule.R
+++ R/data/schedule.R
@@ -6,7 +6,7 @@ library(furrr)
 
 # scrape schedules -------------------------------------------------------------
 
-scrape_schedule <- function(league, date) {
+scrape_schedule <- function(league, date, sleep_time = 1) {
   
   # build url
   url <- glue::glue("https://www.espn.com/{league}-college-basketball/schedule/_/date/{date}")
@@ -18,30 +18,76 @@ scrape_schedule <- function(league, date) {
     html_elements("a") %>%
     html_attr("href")
   
-  Sys.sleep(1)
+  Sys.sleep(sleep_time)
   
   return(games)
   
 }
 
-# scrape & write ---------------------------------------------------------------
+# scrape -----------------------------------------------------------------------
 
 # process results in parallel
 plan(multisession, workers = 8)
 
-schedule <- 
-  crossing(league = c("mens", "womens"),
-           date = seq.Date(from = mdy("11/1/2001"), 
-                           to = mdy("4/30/2024"),
-                           by = "day")) %>%
-  mutate(month = month(date)) %>%
-  filter(!month %in% 5:10) %>%
-  select(-month) %>%
-  mutate(date = str_remove_all(as.character(date), "-")) %>%
-  mutate(game_id = future_pmap(list(league, date), 
-                               ~scrape_schedule(..1, ..2),
-                               .progress = TRUE))
+# breaking this up in chunks to be polite to the robots
+for (league in c("mens", "womens")) {
+  
+  cli::cli_h1(glue::glue("{str_to_title(league)} Basketball"))
+  
+  for (season in 2002:2024) {
+    
+    cli::cli_h2(glue::glue("{season - 1}-{str_sub(as.character(season), -2)}"))
+    
+    # pull games between November & April
+    season_start <- mdy(paste0("11/1/", season - 1))
+    season_end <- mdy(paste0("4/30/", season))
+    
+    # scrape espn's site
+    scraped <-
+      tibble(league = league,
+             date = seq.Date(from = season_start,
+                             to = season_end,
+                             by = "day")) %>%
+      mutate(date = str_remove_all(as.character(date), "-")) %>%
+      mutate(game_id = future_pmap(list(league, date),
+                                   ~scrape_schedule(..1, ..2),
+                                   .progress = TRUE))
+    
+    # create schedule if it doesn't exist already
+    if (!exists("schedule")) {
+      
+      schedule <- scraped
+      
+    } else {
+      
+      schedule <-
+        schedule %>%
+        bind_rows(scraped)
+      
+    }
+    
+    # give 1-min break between scraping seasons
+    cli::cli_progress_bar("Sleeping...", total = 60, clear = FALSE)
+    for (t in 1:60) {
+      Sys.sleep(1)
+      cli::cli_progress_update()
+    }
+    
+  }
+  
+}
 
 plan(sequential)
 
+# wrangle & write --------------------------------------------------------------
+
+schedule %>% 
+  mutate(n = map_int(game_id, length)) %>%
+  filter(n > 0) %>%
+  select(-n) %>%
+  unnest(game_id) %>%
+  mutate(game_id = str_remove_all(game_id, "/mens-|/womens-|college-basketball/game/_/gameId/"),
+         game_id = map_chr(game_id, ~str_split_1(.x, "/")[1]),
+         date = ymd(date)) %>%
+  arrow::write_parquet("data/schedule/schedule.parquet")
 
